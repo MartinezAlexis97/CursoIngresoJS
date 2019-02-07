7d49157de25cbb7a3c2dfd9c59a1e8256c819e68
alumno <alumno>
2019-02-06 22:26:13 -0300

    ifejc2

--- 2-InstruccionIf/jsInstruccionIF (3).js
+++ 2-InstruccionIf/jsInstruccionIF (3).js
@@ -1,5 +1,12 @@
 function mostrar()
 {
+    var edad
+    edad=document.getElementById("edad").value;
+if (edad>=18){
+    alert("mayor de edad")
+}else{
+    alert("menor de edad")
+}
 //tomo la edad  
 
 
