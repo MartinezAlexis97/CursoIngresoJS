abc2d6c3b8463d1cbbad4ebcb2c98d30e8929c8b
alumno <alumno>
2019-02-19 19:20:50 -0300

    parcial5

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/cinco.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/cinco.js
@@ -1,4 +1,32 @@
 function mostrar()
 {
+    var planeta;
 
-}
+        planeta=prompt("Planeta del Sistema Solar (en minúsculas)");
+
+        switch(planeta){
+
+            case "tierra":
+                alert("acá vivimos")
+                break;
+
+            case "mercurio":
+            case "venus":
+                alert("acá hace mas calor")
+                break;
+
+            case "marte":
+            case "jupiter":
+            case "saturno":
+            case "urano":
+            case "neptuno":
+            case "plutón":
+                alert("acá hace mas frío")
+                break;
+
+                default: alert("no es un planeta válido")
+        }
+        
+            
+            
+ }
