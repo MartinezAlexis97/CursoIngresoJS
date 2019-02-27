9e3c6f6e4215bd6156f98ccd218cdbff5d253af3
MartinezAlexis97 <admartinez32@yahoo.com>
2019-02-26 22:18:38 -0300

    ult clase

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/cinco.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/cinco.js
@@ -1,4 +1,51 @@
 function mostrar()
 {
+  var precio,pago,paquete,descuento;
+
+    precio=parseInt(prompt("Valor de habitacion"));
+    pago=prompt("Forma de pago:\n\nVisa (10%desc)\npayPal (15%desc)\nmercadoPago (10%desc)\nEfectivo (20%desc)\nOtro (5%desc)");
+    paquete=prompt("Paquete:\n\nTodo Incluido.\nSolo desayuno.\nNinguno.")
+
+    switch(pago){
+
+        case "Visa":
+        case "mercadoPago":
+            descuento=precio*0.9;
+            break;
+
+        case "payPal":
+            descuento=precio*0.85;
+            switch(paquete){
+
+              case "Todo incluido":
+                descuento=descuento-(precio*0.1)
+                break;
+            }
+             break;
+
+        case "Efectivo":
+            descuento=precio*0.8;
+            switch(paquete){
+
+              case "Todo incluido":
+                descuento=descuento-(precio*0.15)
+                  break;
+
+              case "Solo desayuno":
+                descuento=descuento-(precio*0.1)
+                  break;
+
+            }
+            break;
+
+        case "Otro":
+            descuento=precio*0.95;
+            break;
+    }
+
+    alert("El precio final es: $" +descuento);
+;
+
+
 
 }
--- 9-Parciales/2018_1Cuatri_Recu_fuentes/cuatro.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/cuatro.js
@@ -1,30 +1,73 @@
 function mostrar()
 {
-    var cantidad,precio,total,diez,veinti,recargo,tarjeta,recargo;
+  /*
+    var num1,num2,num3,num4,suma,descuento,max;
 
-        cantidad=prompt("Cantidad de libros")
-        precio=prompt("Precio")
-        tarjeta=prompt("Paga con tarjeta? (si-no)")
-        cantidad=parseInt(cantidad)
-        precio=parseInt(precio)
+    num1=parseInt(prompt("Ingrese primer importe"));
+    num2=parseInt(prompt("Ingrese segundo importe"));
+    num3=parseInt(prompt("Ingrese tercer importe"));
+    num4=parseInt(prompt("Ingrese cuarto importe"));
 
-        diez=precio-(precio*10/100);
-        veinti=precio-(precio*25/100);
+        suma=num1+num2+num3+num4;
+        max=num1
 
-            if(tarjeta=="si"){
-                recargo=precio*0.1;
-                }else{
-                    recargo=0;
-                }
-            if(cantidad>2 && precio>2000){
-                   total=veinti+recargo;
-                   
-                        }else{
-                        
-                            if(cantidad<2 && precio<2001){
-                                total=diez+recargo;
-                                
-             }  
+        if(num2>max){
+            max=num2
+
         }
-           alert("Su precio final es $" +total);
+            if(num3>max){
+              max=num3
+            }
+            if(num4>max){
+              max=num4
+            }
+
+        if(suma>100){
+
+            descuento=suma*0.9;
+
+      }else{
+
+            if(suma<101 && suma>50){
+
+              descuento=suma*0.95;
+
+            }else{
+
+              descuento=suma*1.15;
 }
+      }
+
+
+        alert("el importe final es: $" +descuento+ "\nel importe mayor ingresado es: $" +max);
+    */
+  var num,cont=1,suma=0,max;
+
+          while(cont<=4){
+
+          cont++
+          num=parseInt(prompt("ingrese importe"));
+          suma=suma+num
+
+          if(cont==2){
+            max=num
+          }else{
+            if(num>max){
+              max=num
+            }
+          }
+
+          }
+          if(suma>100){
+            suma=suma*0.9;
+
+          }else{
+            if(suma<101 && suma>50){
+              suma=suma*0.95;
+            }
+            if(suma<51){
+              suma=suma*1.15;
+            }
+}
+            alert("El importe final es: $" +suma+ " El importe mayor es: $" +max);
+  }
